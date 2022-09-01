cc3283d20ea6cda2f87de2089b4d57323c28b924
Dmitry Rakov <rakov@mirapolis.ru>
2022-09-02 01:56:06 +0300

    Классы NFA и DFA

--- automat.js
+++ automat.js
@@ -1,4 +1,20 @@
-const {MapOfMap} = require('./special-map.js');
+const {MapOfMap} = require('@grunmouse/special-map');
+const {
+	symbols:{UNION, INTERSECTION, DIFFERENCE, COMPLEMENT}, 
+	operators
+} = require('@grunmouse/multioperator-set-theory');
+
+
+
+/**
+ * @struct NFA
+ * @property A : Iterable - алфавит
+ * @property Q : Iterable - множество состояний
+ * @property S : Set(Q) - множество начальных состояний S \subset Q
+ * @property T : Set<Q> - множество допускающих состояний T \subset Q
+ * @property d : MapOfMap<Q, A, Set<Q>> - таблица переходов
+ *
+ */
 
 /**
  * @struct DFA
@@ -23,17 +39,60 @@ function cloneTable(t){
 	return d;
 }
 
-
-function cloneDFA(M){
-	const A = new Set(M.A);
-	const Q = new Set(M.Q);
-	const s = M.s;
-	const T = new Set(M.T);
-	const d = new MapOfMap();
-	for(let [q, a, r] of M.d){
-		d.set(q, a, r);
+class FA{
+	constructor(M){
+		this.A = new Set(M.A);
+		this.Q = new Set(M.Q);
+		this.T = new Set(M.T);
+		this.d = cloneTable(M.d);
+		if(M.devil){
+			addDevilStateInto(this);
+		}
 	}
-	return {A, Q, s, T, d};
+}
+
+class DFA extends FA{
+	constructor(M){
+		super(M);
+		this.s = M.s;
+	}
+}
+
+class NFA extends FA{
+	constructor(M){
+		super(M);
+		this.S = new Set(M.S);
+	}
+	
+	/**
+	 * \epsilon-замыкание множества состояний
+	 */
+	epsClosure(S){
+		let queue = [...S];
+		for(let index = 0; index<queue.length; ++index){
+			let q = queue[index];
+			let R = this.d.get(q, '');
+			for(let r of R){
+				if(!queue.includes(r)){
+					queue.push(r);
+				}
+			}
+		}
+		return new Set(queue);
+	}
+	
+	/** 
+	 * переход из множества состояний S по символу a
+	 */
+	next(S, a){
+		let result = [];
+		for(let q of S){
+			let R = this.d.get(q, a);
+			result.push(...R);
+		}
+		result = new Set(result);
+		return epsClosure(result);
+	}
 }
  
 /**
@@ -63,8 +122,9 @@ function makeMultipleTable(A1, A2){
 }
 
 /**
- * Создаёт множество, состоящее из попарных произведений членов множеств T1 и T2
- * @param table : MapOfMap<Q1, Q2, Q>
+ * Создаёт множество, состоящее из попарных произведений членов множеств T1 и T2,  
+ *    с использованием заранее сгенерированной таблицы произведений
+ * @param table : MapOfMap<Q1, Q2, Q> - таблица произведений
  * @param T1 : Iterable<Q1>
  * @param T2 : Iterable<Q2>
  * @return Set<Q>
@@ -89,6 +149,8 @@ function multipleSetWithTable(table, T1, T2){
  * @param d2 : MapOfMap<Q2, A, Q2>
  *
  * @return MapOfMap<Q, A, Q>
+ *
+ * d([q1, q2], a) = [d1(q1, a), d2(q2, a)]
  */
 function multipleAutomatTable(table, invert, Q, A, d1, d2){
 	const d = new MapOfMap();
@@ -119,7 +181,7 @@ function multipleDFA(A1, A2){
 	const d = multipleAutomatTable(table, invert, Q, A, A1.d, A2.d); //
 	
 	return {
-		A:{A, Q, s, T, d},
+		A:new DFA({A, Q, s, T, d}),
 		table, invert
 	};
 }
@@ -165,29 +227,18 @@ function addDevilStateInto(A){
 	return A;
 }
 
-
-/**
- * @param Q : Iterable;
- * @param T : Set<Q>
- */
-function complementSet(Q, T){
-	return new Set([...Q].fulter(q=>(!T.has(q)))); //Все члены, Q, не входящие в T
-}
-
-function joinSet(A, B){
-	return new Set([...A, ...B]);
-}
-
 /**
  * Дополнение конечного автомата
  */
 function complementDFA(A){
-	let C = cloneDFO(A);
+	let C = new DFA(A);
 	C = addDevilStateInto(C);
-	C.T = complementSet(C.Q, C.T); 
+	C.T = C.Q[DIFFERENCE](C.T); 
 	
 	return C;
 }
+
+operation.complement.def(DFA, complementDFA);
 
 /**
  * Объединение конечных автоматов
@@ -195,13 +246,16 @@ function complementDFA(A){
 function joinDFA(A1, A2){
 	const {A, table, invert} = multipleDFA(A1, A2);
 	
-	A.T = joinSet(
-		multipleSetWithTable(table, A1.T, A2.Q),
+	A.T = multipleSetWithTable(table, A1.T, A2.Q)[UNION](
 		multipleSetWithTable(table, A1.Q, A2.T)
 	);
 	
 	return A;
 }
+
+operation.union.def(DFA, DFA, joinDFA);
+
+
 
 /**
  * Пересечение конечных автоматов
@@ -211,9 +265,168 @@ function intersectDFA(A1, A2){
 	return A;
 }
 
+operation.intersection.def(DFA, DFA, intersectDFA);
+
 /**
  * Разность конечных автоматов
  */
 function subtraceDFA(A, B){
 	return intersectDFA(A, complementDFA(B));
+}
+
+operation.difference.def(DFA, DFA, subtraceDFA );
+
+
+function findEquivalenceClasses(M){
+	const classes = new Set([M.T, M.Q[DIFFERENCE](M.T)]);
+	const A = M.A;
+	const d = M.d;
+	const queue = [];
+	for(let a of A){
+		for(let cls of classes){
+			queue.push([cls, a]);
+		}
+	}
+	let index = 0;
+	for(; index < queue.length; ++index){
+		let [C, a] = queue[index];
+		if(!classes.has(C)) continue;
+		for(let R of classes){
+			let R1 = new Set();
+			let R2 = new Set();
+			for(let q of R){
+				if(C.has(d.get(q, a))){
+					R1.add(q);
+				}
+				else{
+					R2.add(q);
+				}
+			}
+			if(R1.size >0 && R2.size>0){
+				classes.delete(R);
+				classes.add(R1);
+				classes.add(R2);
+				for(let a of A){
+					queue.push([R1, a], [R2, a]);
+				}
+			}
+		}
+	}
+	
+	let classesMap = new Map();
+	for(let cls of classes){
+		let sym = Symbol();
+		for(let q of cls){
+			classesMap.set(q, sym);
+		}
+	}
+	
+	return classesMap;
+}
+
+function reachableStates(A){
+	let Q = new Set([A.s]);
+	let count = Q.size;
+	
+	do{
+		count = Q.size;
+		for(let q of Q){
+			for(let [a, r] of A.d.get(q)){
+				Q.add(r);
+			}
+		}
+	
+	}while(Q.size > count)
+	
+	return Q;
+}
+
+
+function minimizeDFA(A){
+	let Q = reachableStates(A);
+	let d = new MapOfMap(Array.from(Q, (q)=>([q, A.d.get(q)])));
+	//Q - достижимые состояния, d - таблица перехода для достижимых состояний
+	
+	let classesMap = findEquivalenceClasses({Q, d, A.A, A.T});
+	
+	let d1 = new MapOfMap();
+	for(let [q, a, r] of d){
+		q1 = classesMap.get(q);
+		r1 = classesMap.get(r);
+		d1.set(q1, a, r1);
+	}
+	let Q1 = classesMap.values();
+	
+	return new DFA({Q:Q1, d:d1, A:A.A, s:A.s, T:Q1[INTERSECTION](A.T));
+}
+
+
+/**
+ * Реверсирует детерминированный конечный автомат
+ * @param A : DFA
+ * @return NFA
+ */
+function reverseDFA(A){
+	const d1 = new MapOfMap();
+	
+	for(let [q, a, r] of A.d){
+		let s = d1.get(r, a);
+		if(!s){
+			s = new Set([q]);
+			d1.set(r, a, s);
+		}
+		else{
+			s.add(q);
+		}
+	}
+	
+	return new NFA({A:A.A, Q:A.Q, d:d1, S:A.T, T:[A.s]});
+}
+
+/**
+ * Строит детерминиированый КА по недетерминированному
+ */
+function determineNFA(A){
+	const qOrder = [...A.Q[DIFFERENCE](A.T)];
+	const countN = qOrder.length;
+	qOrder.push(A.T);
+	
+	/**
+	 * Кодовый номер множества состояний
+	 * @param S : Set<Q>
+	 * @return BigInt
+	 */
+	function qIndex(S){
+		let index = 0n;
+		for(let q of S){
+			let i = qOrder.indexOf(q);
+			index += 2n**BigInt(i);
+		}
+	}
+	
+	function isFinal(index){
+		return index >= 2n**BigInt(countN);
+	}
+	
+	const queue = [A.epsClosure(A.S)];
+	const s = qIndex(queue[0]);
+	const Q = new Set([qIndex(A.S)]);
+	const d1 = new MapOfMap();
+	for(let index = 0; index<=queue; ++index){
+		let S = queue[index];
+		let q = qIndex(S);
+		for(let a of A.A){
+			let R = A.next(S, A);
+			let r = qIndex(R);
+			d1.set(q, a, r);
+			if(!Q.has(r)){
+				queue.push(R);
+				Q.add(r);
+			}
+		}
+	}
+	
+	const T = [...Q].filter(isFinal);
+	
+	return new DFA({Q, A:A.A, T, s, d:d1});
 }
